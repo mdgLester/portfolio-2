4c73cac5c6b4f3c616fc2dcf712984018d5081f0
Lester <deguzman.lester.bsit@gmail.com>
2026-03-07 14:38:31 +0800

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,7 +3,7 @@ import RevealOnScroll from './RevealOnScroll';
 import '../styles/Skills.css';
 
 const Skills = () => {
-  const skills = ["Microsoft Office","Basic IT","Problem Solving","Teamwork","Analytical Thinking","Responsive Web Design","React.js","Node.js","Git & GitHub","MongoDB","MySQL","HTML","CSS","JavaScript","Robot Framework","Relational Databases","Cisco Packet Tracer",""];
+  const skills = ["Microsoft Office","Basic IT","Problem Solving","Teamwork","Analytical Thinking","Responsive Web Design","React.js","Node.js","Git & GitHub","MongoDB","MySQL","HTML","CSS","JavaScript","Robot Framework","Relational Databases","Cisco Packet Tracer","Server Management","Network Troubleshooting","Technical Support","Communication Skills","Time Management","Adaptability",];
   return (
     <section id="skills" className="skills">
       <RevealOnScroll direction="fade"><h2>Skills</h2></RevealOnScroll>
